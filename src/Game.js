4bf687db1e8feb095195fd3b927f408cfac63f00
Charlee Li <oda.charlee@gmail.com>
2018-05-04 01:14:02 -0400

    changed var name

--- src/Game.js
+++ src/Game.js
@@ -108,15 +108,15 @@ class Game extends React.Component {
 
         for (let y = 0; y < this.rows; y++) {
             for (let x = 0; x < this.cols; x++) {
-                let neighbours = this.calculateNeighbours(this.board, x, y);
+                let neighbors = this.calculateNeighbors(this.board, x, y);
                 if (this.board[y][x]) {
-                    if (neighbours === 2 || neighbours === 3) {
+                    if (neighbors === 2 || neighbors === 3) {
                         newBoard[y][x] = true;
                     } else {
                         newBoard[y][x] = false;
                     }
                 } else {
-                    if (!this.board[y][x] && neighbours === 3) {
+                    if (!this.board[y][x] && neighbors === 3) {
                         newBoard[y][x] = true;
                     }
                 }
@@ -132,13 +132,13 @@ class Game extends React.Component {
     }
 
     /**
-     * Calculate the number of neighbours at point (x, y)
+     * Calculate the number of neighbors at point (x, y)
      * @param {Array} board 
      * @param {int} x 
      * @param {int} y 
      */
-    calculateNeighbours(board, x, y) {
-        let neighbours = 0;
+    calculateNeighbors(board, x, y) {
+        let neighbors = 0;
         const dirs = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
         for (let i = 0; i < dirs.length; i++) {
             const dir = dirs[i];
@@ -146,11 +146,11 @@ class Game extends React.Component {
             let x1 = x + dir[1];
 
             if (x1 >= 0 && x1 < this.cols && y1 >= 0 && y1 < this.rows && board[y1][x1]) {
-                neighbours++;
+                neighbors++;
             }
         }
 
-        return neighbours;
+        return neighbors;
     }
 
     handleIntervalChange = (event) => {
